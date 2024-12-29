6244520466e54d11452bf37de5f919843bf6ee1d
h <>
2024-12-29 21:17:46 +0200

    xsc

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Shay Shimony on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
