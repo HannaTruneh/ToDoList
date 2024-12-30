cec90e19d807044fb3cc5220b9286a76a712f84e
h <>
2024-12-30 09:50:56 +0200

    no message

---
+++ ToDoListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListDetailView.swift
+//  ToDoList
+//
+//  Created by Hanna Truneh on 30/12/2024.
+//
+
+import SwiftUI
+
+struct ToDoListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListDetailView()
+}
